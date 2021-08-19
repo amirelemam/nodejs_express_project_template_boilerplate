5d7846096bd9970567f020d756e94aeab81dd863
Amir Elemam <amirelemam@gmail.com>
2021-08-19 01:47:19 -0300

    fix linting

--- src/common/errors.js
+++ src/common/errors.js
@@ -5,22 +5,16 @@ class GerericError extends Error {
   }
 }
 
-module.exports.BadRequestError = (msg = "Bad Request") =>
-  new GerericError(msg, 400);
+module.exports.BadRequestError = (msg = 'Bad Request') => new GerericError(msg, 400);
 
-module.exports.UnauthorizedError = (msg = "Unauthorized") =>
-  new GerericError(msg, 401);
+module.exports.UnauthorizedError = (msg = 'Unauthorized') => new GerericError(msg, 401);
 
-module.exports.ForbiddenError = (msg = "Forbidden") =>
-  new GerericError(msg, 403);
+module.exports.ForbiddenError = (msg = 'Forbidden') => new GerericError(msg, 403);
 
-module.exports.NotFoundError = (msg = "Not Found") =>
-  new GerericError(msg, 404);
+module.exports.NotFoundError = (msg = 'Not Found') => new GerericError(msg, 404);
 
-module.exports.ConflictError = (msg = "Conflict") => new GerericError(msg, 409);
+module.exports.ConflictError = (msg = 'Conflict') => new GerericError(msg, 409);
 
-module.exports.UnprocessableEntityError = (msg = "Unprocessable Entity") =>
-  new GerericError(msg, 422);
+module.exports.UnprocessableEntityError = (msg = 'Unprocessable Entity') => new GerericError(msg, 422);
 
-module.exports.InternalServerError = (msg = "Internal Server Error") =>
-  new GerericError(msg, 500);
+module.exports.InternalServerError = (msg = 'Internal Server Error') => new GerericError(msg, 500);
