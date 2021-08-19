219e5c49dc67b22e1dd164dd7b42a0221986aa4e
Amir Elemam <amirelemam@gmail.com>
2021-08-19 01:46:56 -0300

    update packages

--- src/common/errors.js
+++ src/common/errors.js
@@ -1,34 +1,26 @@
-const logger = require('./logger');
-
 class GerericError extends Error {
   constructor(msg, status) {
     super(msg);
-
-    this.log = () => logger.error(this.message);
     this.status = status;
   }
 }
 
-exports.ConflictError = (
-  msg = 'Request data may have fields that are not unique, not updatable or are already registered.',
-) => new GerericError(msg, 409);
+module.exports.BadRequestError = (msg = "Bad Request") =>
+  new GerericError(msg, 400);
 
-exports.InternalServerError = (msg = 'Internal Server Error.') => new GerericError(msg, 500);
+module.exports.UnauthorizedError = (msg = "Unauthorized") =>
+  new GerericError(msg, 401);
 
-exports.BadRequestError = (
-  msg = 'Missing fields or input in incorrect format.',
-) => new GerericError(msg, 400);
+module.exports.ForbiddenError = (msg = "Forbidden") =>
+  new GerericError(msg, 403);
 
-exports.NotFoundError = (
-  msg = 'The route or data in your request cannot be found.',
-) => new GerericError(msg, 404);
+module.exports.NotFoundError = (msg = "Not Found") =>
+  new GerericError(msg, 404);
 
-exports.UnauthorizedError = (msg = 'access-token is invalid or expired.') => new GerericError(msg, 401);
+module.exports.ConflictError = (msg = "Conflict") => new GerericError(msg, 409);
 
-exports.UnauthenticatedError = (msg = 'Invalid username or password.') => new GerericError(msg, 401);
+module.exports.UnprocessableEntityError = (msg = "Unprocessable Entity") =>
+  new GerericError(msg, 422);
 
-exports.ForbiddenError = (
-  msg = 'you are not allowed to access to this resource or perform this action.',
-) => new GerericError(msg, 403);
-
-exports.UnprocessableEntityError = (msg = 'Unable to perform this action.') => new GerericError(msg, 422);
+module.exports.InternalServerError = (msg = "Internal Server Error") =>
+  new GerericError(msg, 500);
